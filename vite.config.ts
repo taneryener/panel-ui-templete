ae0c4c944041660114a9fe0309729d31ca443bbf
Taner Yener <taneryener@192.168.2.125>
2025-02-23 10:35:36 +0300

    fix for configs about postcss+tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: 'localhost',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
   },
-  plugins: [react(), tailwindcss()],
 });
