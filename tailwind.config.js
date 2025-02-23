750f1990e1e9a98176a8c0afc2be113948800d89
Taner Yener <taneryener@192.168.2.125>
2025-02-23 07:00:00 +0300

    added configs for tailwind postcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './index.html',        // Include HTML files
+    './src/**/*.{js,ts,jsx,tsx}',  // Include React components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
